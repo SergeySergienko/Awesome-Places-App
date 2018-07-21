53745f25db2b05b4a5dac4f7ce4f301c71d1eb6b
SergeySergienko <serg.v.sergienko@gmail.com>
2018-07-21 21:30:39 +0300

    add Animation

--- src/screens/FindPlaceScreen.js
+++ src/screens/FindPlaceScreen.js
@@ -1,24 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Animated
+} from "react-native";
 import { connect } from "react-redux";
 import PlaceList from "../components/PlaceList/PlaceList";
 
 class FindPlaceScreen extends React.Component {
+  state = {
+    placesLoaded: false,
+    removeAnim: new Animated.Value(1)
+  };
+  placesSearchHandler = () => {
+    Animated.timing(this.state.removeAnim, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true
+    }).start();
+    // this.setState({
+    //   placesLoaded: true
+    // });
+  };
   itemSelectedHandler = key => {
-    const selPlace = this.props.places.find(place => place.key === key);
-    this.props.navigation.push("Detail", {
-      selectedPlace: selPlace
-    });
+    const selectedPlace = this.props.places.find(place => place.key === key);
+    this.props.navigation.push("Detail", { selectedPlace });
   };
   render() {
-    return (
-      <View style={styles.container}>
+    let content = (
+      <Animated.View
+        style={{
+          opacity: this.state.removeAnim,
+          transform: [
+            {
+              scale: this.state.removeAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [12, 1]
+              })
+            }
+          ]
+        }}
+      >
+        <TouchableOpacity onPress={this.placesSearchHandler}>
+          <View style={styles.searchButton}>
+            <Text style={styles.searchButtonText}>Find Places</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+    if (this.state.placesLoaded) {
+      content = (
         <PlaceList
           places={this.props.places}
           onItemSelected={this.itemSelectedHandler}
         />
-      </View>
-    );
+      );
+    }
+    return <View style={styles.container}>{content}</View>;
   }
 }
 
@@ -27,6 +67,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  searchButton: {
+    borderColor: "orange",
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: "orange",
+    fontWeight: "bold",
+    fontSize: 26
   }
 });
 
